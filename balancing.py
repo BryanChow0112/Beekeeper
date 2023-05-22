b394e1912d6fd93b65082ccaf4dcda658fcd75ce
wkiy0001 <126316006+wkiy0001@users.noreply.github.com>
2023-05-22 11:07:39 +0800

    Update balancing.py

--- balancing.py
+++ balancing.py
@@ -6,50 +6,84 @@ from math import floor
 
 
 def make_ordering(my_coordinate_list: list[Point]) -> list[Point]:
+    """
+    complexity: O(N * log(N)) where N is the number of element in input list
+    """
     res = []
+
     def make_ordering_inner(my_list):
         if len(my_list) <= 17:
             res.extend(my_list)
+            return
         else:
-            n = len(my_list)
-            pivot = floor((n + 7) / 8) / n
+            a = 12.5
 
             p_x = Percentiles()
-            for point in my_list:
-                p_x.add_point(point[0])
-
-            res_x = p_x.ratio(pivot, pivot)
-
             p_y = Percentiles()
-            for point in my_list:
-                p_y.add_point(point[1])
-
-            res_y = p_y.ratio(pivot, pivot)
-
             p_z = Percentiles()
+
             for point in my_list:
+                p_x.add_point(point[0])  # O(log(N))
+                p_y.add_point(point[1])
                 p_z.add_point(point[2])
 
-            res_z = p_z.ratio(pivot, pivot)
+            res_x = p_x.ratio(a, a)
+            res_y = p_y.ratio(a, a)  # O(log(N) + O)
+            res_z = p_z.ratio(a, a)
+
+            current_point = None
 
             for point in my_coordinate_list:
                 if point[0] in res_x and point[1] in res_y and point[2] in res_z:
+                    current_point = point
                     res.append(point)
-                    my_list.remove(point)
+                    my_list.remove(point)  # O(log(N))
                     break
 
-            mid = len(my_list)//2
+            octant1 = []
+            octant2 = []
+            octant3 = []
+            octant4 = []
+            octant5 = []
+            octant6 = []
+            octant7 = []
+            octant8 = []
 
-            return make_ordering_inner(my_list[:mid])
-            return make_ordering_inner(my_list[mid:])
+            for point in my_list:
+                if point[0] < current_point[0]:
+                    if point[1] < current_point[1]:
+                        if point[2] < current_point[2]:
+                            octant1.append(point)
+                        else:
+                            octant2.append(point)
+                    else:
+                        if point[2] < current_point[2]:
+                            octant3.append(point)
+                        else:
+                            octant4.append(point)
+                else:
+                    if point[1] < current_point[1]:
+                        if point[2] < current_point[2]:
+                            octant5.append(point)
+                        else:
+                            octant6.append(point)
+                    else:
+                        if point[2] < current_point[2]:
+                            octant7.append(point)
+                        else:
+                            octant8.append(point)
+
+            make_ordering_inner(octant1)
+            make_ordering_inner(octant2)
+            make_ordering_inner(octant3)
+            make_ordering_inner(octant4)
+            make_ordering_inner(octant5)
+            make_ordering_inner(octant6)
+            make_ordering_inner(octant7)
+            make_ordering_inner(octant8)
 
     make_ordering_inner(my_coordinate_list)
     return res
-
-
-
-
-
 
 if __name__ == "__main__":
     my_coordinate_list = [
